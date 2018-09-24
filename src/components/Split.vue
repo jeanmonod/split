54945000806c981efd401932075e6fd4bd38960e
David Jeanmonod <david.jeanmonod@gmail.com>
2018-09-24 22:06:10 +0200

    Allow to add/remove participants

--- src/components/Split.vue
+++ src/components/Split.vue
@@ -2,17 +2,36 @@
   <div class="split">
     <h1>Split</h1>
     <ul>
-      <li v-for="p in this.$store.state.participants">{{ p }}</li>
+      <li v-for="(p, index) in this.$store.state.participants">
+        {{ p }} <button @click="removeParticipant(index)">X</button>
+      </li>
     </ul>
+    <label for="newParticipantInput">New participant:</label>
+    <input id="newParticipantInput" @keyup.enter="addParticipant()" v-model="newName" placeholder="name + ENTER">
   </div>
 </template>
 
 <script>
 export default {
   name: 'Split',
+  data: function () {
+    return {
+      newName: ''
+    }
+  },
   props: {
-    msg: String
-  }
+  },
+  methods: {
+    removeParticipant(position) {
+      this.$store.dispatch('removeParticipant', position);
+    },
+    addParticipant() {
+      if (this.newName.length > 0) {
+        this.$store.dispatch('addParticipant', this.newName);
+      }
+      this.newName = '';
+    }
+  },
 }
 </script>
 
@@ -20,4 +39,5 @@ export default {
 h1 {
   color: red;
 }
+
 </style>
--- src/store.js
+++ src/store.js
@@ -8,9 +8,19 @@ export default new Vuex.Store({
     participants: ['Boutiller', 'Zuffrey', 'Alex', 'Seb', 'Damien', 'Frank', 'Ben', 'Cedric', 'Sylvain', 'David']
   },
   mutations: {
-
+    addParticipant (state, name) {
+      state.participants.push(name);
+    },
+    removeParticipant(state, position) {
+      state.participants.splice(position, 1);
+    }
   },
   actions: {
-
+    addParticipant(context, name) {
+      context.commit('addParticipant', name);
+    },
+    removeParticipant(context, position) {
+      context.commit('removeParticipant', position);
+    }
   }
 })
