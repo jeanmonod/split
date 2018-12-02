6d79f5ff5e92fe1c422ad9bc48bb08472b176530
David Jeanmonod <david.jeanmonod@gmail.com>
2018-12-02 22:09:28 +0100

    Remove fixtures

--- src/store/index.js
+++ src/store/index.js
@@ -11,22 +11,9 @@ Vue.use(Vuex);
 let initialState = {
   version: 0.1,
   queryString: null,
-  participants: {
-    1: {id: 1, name: 'Ben'},
-    2: {id: 2, name: 'Cédric'},
-    3: {id: 3, name: 'Sylvain'},
-    4: {id: 4, name: 'David'}
-  },
-  groups: {
-    1: {id: 1, name: 'Achats'},
-    2: {id: 2, name: 'Hotels'},
-    3: {id: 3, name: 'Avions'}
-  },
-  expenses: {
-    1: {id: 1, amount: 230.5, participant: 2, group: 2, name: 'Facture booking.com'},
-    2: {id: 2, amount: 17.90, participant: 4, group: 1, name: 'Bières à l\'aéroport'},
-    3: {id: 3, amount: 109.0, participant: 2, group: 2, name: 'Hôtel nuit supplémentaire'}
-  },
+  participants: {},
+  groups: {},
+  expenses: {},
   refunds: {
     resolver: null,
     balances: {},
